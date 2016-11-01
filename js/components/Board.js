bf8a2c124150dc92356bf2a6a52a06b4004f6b67
Benjamin Soung <soungbenjamin@gmail.com>
2016-11-01 16:44:58 -0400

    minor fixes

--- js/components/Board.js
+++ js/components/Board.js
@@ -5,6 +5,7 @@ const Board = (props) => (
 	<div className='board'>
 		<h3 className='board-title'>{props.title}</h3>
 		{/* <List title='List title' cards={["Card 1", "Card 2", "muahhh"]}/> */}
+
 		{props.lists.map(list => <List title={list} cards={["Card 1", "Card 2", "muahhh"]} /> )}
 	</div>
 )
--- js/index.js
+++ js/index.js
@@ -7,6 +7,6 @@ const Card = require('./components/Card')
 const Board = require('./components/Board')
 
 document.addEventListener('DOMContentLoaded', function() {
-	ReactDOM.render(<Board title='board 1' lists={['list 1', 'list 2']}/>, document.getElementById('app'))
+	ReactDOM.render(<Board title='board 1' lists={['Duck', 'Duck', 'Duck', 'GOOSE']}/>, document.getElementById('app'))
 })
 
