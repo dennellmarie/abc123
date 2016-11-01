167fa66c31b6d29cefe18380904b200502958c9f
Benjamin Soung <soungbenjamin@gmail.com>
2016-11-01 16:34:01 -0400

    add more props

--- js/components/Board.js
+++ js/components/Board.js
@@ -3,7 +3,9 @@ const List = require('./List')
 
 const Board = (props) => (
 	<div className='board'>
-		<List title='List title' cards={["Card 1", "Card 2", "muahhh"]}/>
+		<h3 className='board-title'>{props.title}</h3>
+		{/* <List title='List title' cards={["Card 1", "Card 2", "muahhh"]}/> */}
+		{props.lists.map(list => <List title={list} cards={["Card 1", "Card 2", "muahhh"]} /> )}
 	</div>
 )
 
--- js/index.js
+++ js/index.js
@@ -6,8 +6,7 @@ const List = require('./components/List')
 const Card = require('./components/Card')
 const Board = require('./components/Board')
 
-
 document.addEventListener('DOMContentLoaded', function() {
-	ReactDOM.render(<Board />, document.getElementById('app'))
+	ReactDOM.render(<Board title='board 1' lists={['list 1', 'list 2']}/>, document.getElementById('app'))
 })
 
